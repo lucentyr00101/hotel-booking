97f045f76ffdf921a16f784ca345d2286b755ee3
Lucentyr00101 <sarcosredilbertranz@gmail.com>
2019-04-30 15:12:50 +0800

    revised receipt

--- app/Http/Controllers/InvoiceController.php
+++ app/Http/Controllers/InvoiceController.php
@@ -5,12 +5,14 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Customer;
 use App\RoomsCustomers;
+use PDF;
 
 class InvoiceController extends Controller
 {
     public function generate($pivot_id) {
-        $data                 = RoomsCustomers::findOrFail(decrypt($pivot_id));
-        $pdf = app('dompdf.wrapper')->loadView('invoice.generate', ['data' => $data]);
+        $data = RoomsCustomers::findOrFail(decrypt($pivot_id));
+        $customPaper = array(0,0,607.00,283.80);
+        $pdf  = PDF::loadView('invoice.generate', ['data' => $data])->setPaper($customPaper, 'landscape');
         $type = 'stream';
         if ($type == 'stream') {
             return $pdf->stream('invoice.pdf');
