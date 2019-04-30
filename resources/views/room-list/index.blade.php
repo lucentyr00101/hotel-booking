1780950d296bfc90197299c3e7c403651adcab72
Lucentyr00101 <sarcosredilbertranz@gmail.com>
2019-04-30 11:22:50 +0800

    room list revision

--- resources/views/room-list/index.blade.php
+++ resources/views/room-list/index.blade.php
@@ -6,35 +6,19 @@
         <div class="row">
             @foreach ($rooms as $room)
                 <div class="col-xl-2 pb-4">
-                    @forelse ($room->customers as $customer)
-                        <!-- Card -->
-                        <div class="card">
-                            <!-- Card content -->
-                            <div class="card-body red lighten-3">
-                                <!-- Title -->
-                                <h4 class="card-title"><a>{{ $room->type_of_room }} ({{ $room->max_available_rooms - $room->customers()->count() }} room/s left)</a></h4>
-                                <!-- Text -->
-                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                <!-- Button -->
-                                <a href="#" class="btn btn-primary">Button</a>
-                            </div>
+                    <!-- Card -->
+                    <div class="card">
+                        <!-- Card content -->
+                        <div class="card-body red lighten-3">
+                            <!-- Title -->
+                            <h4 class="card-title"><a>{{ $room->type_of_room }} ({{ $room->max_available_rooms - $room->customers()->count() }} room/s left)</a></h4>
+                            <!-- Text -->
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <!-- Button -->
+                            <a href="#" class="btn btn-primary">Button</a>
                         </div>
-                        <!-- Card -->
-                    @empty
-                        <!-- Card -->
-                        <div class="card green lighten-3">
-                            <!-- Card content -->
-                            <div class="card-body">
-                                <!-- Title -->
-                            <h4 class="card-title"><a>{{ $room->type_of_room }} ({{ $room->max_available_rooms }} room/s left)</a></h4>
-                                <!-- Text -->
-                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                <!-- Button -->
-                                <a href="#" class="btn btn-primary">Button</a>
-                            </div>
-                        </div>
-                        <!-- Card -->
-                    @endforelse
+                    </div>
+                    <!-- Card -->
                 </div>
             @endforeach
         </div>
